65af819312534cb737f093aba8c9e78faed9517c
Anton Stasheuski <antonstashevski16@gmail.com>
2020-05-25 17:04:20 +0300

    Change Result to TestsUser jointable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200522173145) do
+ActiveRecord::Schema.define(version: 20200525140020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,15 +38,6 @@ ActiveRecord::Schema.define(version: 20200522173145) do
     t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
-  create_table "results", force: :cascade do |t|
-    t.bigint "test_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["test_id"], name: "index_results_on_test_id"
-    t.index ["user_id"], name: "index_results_on_user_id"
-  end
-
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "difficulty", default: 0, null: false
@@ -54,6 +45,13 @@ ActiveRecord::Schema.define(version: 20200522173145) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
+  end
+
+  create_table "tests_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "test_id", null: false
+    t.index ["test_id", "user_id"], name: "index_tests_users_on_test_id_and_user_id"
+    t.index ["user_id", "test_id"], name: "index_tests_users_on_user_id_and_test_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -66,7 +64,5 @@ ActiveRecord::Schema.define(version: 20200522173145) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
-  add_foreign_key "results", "tests"
-  add_foreign_key "results", "users"
   add_foreign_key "tests", "categories"
 end
