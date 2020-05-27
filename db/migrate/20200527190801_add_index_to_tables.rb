6dd31b5cc06527277fc484fa0757e28a418b22c5
Anton Stasheuski <antonstashevski16@gmail.com>
2020-05-27 23:15:20 +0300

    Fixes #2

--- db/migrate/20200527190801_add_index_to_tables.rb
+++ db/migrate/20200527190801_add_index_to_tables.rb
@@ -1,5 +1,6 @@
 class AddIndexToTables < ActiveRecord::Migration[5.1]
   def change
+    rename_column :categories, :name, :title
     add_index :categories, :title, unique: true
     add_index :users, :email, unique: true
     add_index :tests, %i[title difficulty], unique: true
