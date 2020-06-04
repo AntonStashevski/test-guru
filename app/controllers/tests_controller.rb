b19458ec7234e199ad39513b678b91d754bdfd58
Anton Stasheuski <antonstashevski16@gmail.com>
2020-06-04 15:46:57 +0300

    add index and show actions to test_controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,2 +1,17 @@
 class TestsController < ApplicationController
+
+  def index
+    @tests = Test.all
+  end
+
+  def show
+    find_test
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
 end
