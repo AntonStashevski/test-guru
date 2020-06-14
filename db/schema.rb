e09ee418c027b9008c3400388850e15c6b05d673
Anton Stasheuski <antonstashevski16@gmail.com>
2020-06-14 16:51:01 +0300

    change application.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200614124959) do
+ActiveRecord::Schema.define(version: 20200614133931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,7 +64,6 @@ ActiveRecord::Schema.define(version: 20200614124959) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
     t.integer "role", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
