5c841fdc910b1661d288960fe61ec491898a32ff
Anton Stasheuski <antonstashevski16@gmail.com>
2020-06-26 13:45:49 +0300

    rails-i18n setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :set_locale
+
   def index # перенаправление администраторов на страницу /admin/tests
     case current_user&.class.name
     when 'Admin'
@@ -12,8 +14,18 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def default_url_options
+    { lang: I18n.locale }
+  end
+
   def after_sign_in_path_for(resource) # resource это объект класса user
     resource.admin? ? admin_tests_path : tests_path
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 
 end
