b3de2865d6f976ddfeec76c59d506899e9c81ea3
Anton Stasheuski <antonstashevski16@gmail.com>
2020-07-01 22:58:24 +0300

    use truncate instead validate_body method

--- app/helpers/gists_helper.rb
+++ app/helpers/gists_helper.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 module GistsHelper
-  DEFAULT_LENGTH = 24
+  DEFAULT_LENGTH = 25
 
   def validate_gist_question_body(gist)
-    link_to validate_body(gist.question), admin_test_question_path(test_id: gist.question.test.id, id: gist.question.id)
+    link_to gist.question.body.truncate(DEFAULT_LENGTH), admin_test_question_path(test_id: gist.question.test.id, id: gist.question.id)
   end
 
-  def validate_body(question)
-    question.body.length >= DEFAULT_LENGTH ? "#{question.body[0..DEFAULT_LENGTH - 3]}..." : question.body
-  end
 end
